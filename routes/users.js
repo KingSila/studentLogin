6bcc113ee9bd73ef497a2c310b198d65d85a72f3
Silas Mokone <Silas.Mokone@vodacom.co.za>
2021-11-10 17:50:25 +0200

    fixed also folder name

---
+++ routes/users.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+//login handle
+router.get('/login', (req, res) => {
+    res.render('login');
+})
+router.get('/register', (req, res) => {
+    res.render('register');
+})
+//Register handle
+router.post('/register',(req, res)=>{
+
+})
+router.post('/login',(req, res)=>{
+
+})
+
+//logout
+router.get('/logout',(req, res)=>{
+
+})
+module.exports = router;
