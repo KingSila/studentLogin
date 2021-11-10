89b4525951f96a1c52f0eb848ec8412b1a3c1451
Silas Mokone <Silas.Mokone@vodacom.co.za>
2021-11-10 18:14:48 +0200

    added the main js file

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const app = express();const mongoose = require('mongoose');
+const expressEjsLayout = require('express-ejs-layout');
+//mongoose
+mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true, useUnifiedTopology : true})
+.then(() => console.log('connected,,'))
+.catch((err)=> console.log(err));
+//ejs
+app.set('view engine', 'ejs');
+app.use(expressEjsLayout);
+//BodyParser
+app.use(express.urlencoded({extended:false}));
+
+//Routes
+app.use('/',require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+
+app.listen(3000);
