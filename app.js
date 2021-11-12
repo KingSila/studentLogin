371653ebaddb97b8415112779482410c3e1759d9
Silas Mokone <Silas.Mokone@vodacom.co.za>
2021-11-12 16:40:27 +0200

    added few more lines

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const app = express();const mongoose = require('mongoose');
-const expressEjsLayout = require('express-ejs-layout');
+const expressEjsLayout = require('express-ejs-layouts');
 //mongoose
 mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true, useUnifiedTopology : true})
 .then(() => console.log('connected,,'))
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const User = require('../models/users.js');
 const express = require('express');
 const router = express.Router();
 
@@ -10,8 +11,43 @@ router.get('/register', (req, res) => {
 })
 //Register handle
 router.post('/register',(req, res)=>{
+    const {name,email, password, password2} = req.body;
+    let errors = [];
+    console.log(' Name ' + name+ ' email :' + email+ ' pass:' + password);
+    if(!name || !email || !password || !password2) {
+        errors.push({msg : "Please fill in all fields"})
+    }
+    //check if match
+    if(password !== password2) {
+        errors.push({msg : "passwords dont match"});
+    }
+    
+    //check if password is more than 6 characters
+    if(password.length < 6 ) {
+        errors.push({msg : 'password atleast 6 characters'})
+    }
+    if(errors.length > 0 ) {
+    res.render('register', {
+        errors : errors,
+        name : name,
+        email : email,
+        password : password,
+        password2 : password2})
+    } else {
+        //validation passed
+       User.findOne({email : email}).exec((err,user)=>{
+        console.log(user);   
+        if(user) {
+            errors.push({msg: 'email already registered'});
+            render(res,errors,name,email,password,password2);
+            
+           } else {
+            const newUser = new User({
+                name : name,
+                email : email,
+                password : password
+            });
 
-})
 router.post('/login',(req, res)=>{
 
 })
